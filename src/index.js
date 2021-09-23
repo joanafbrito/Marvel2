87cf60df6c88300468561cea1656598a7bd8becf
Joana Brito <joanafiorentini@hotmail.com>
2021-09-22 23:56:34 -0700

    Fix file path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -15,3 +15,11 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// const router =
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+  
+
+// ReactDOM.render(router, document.getElementById('root'));
