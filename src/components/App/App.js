99c83fc442415c6572f295bf86715282cd53fed7
Joana Brito <joanafiorentini@hotmail.com>
2021-09-24 18:31:04 -0700

    Create utils folder with api file and imported to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 // import logo from '../logo.svg';
 import './App.css';
+// import { getData } from '../../utils/apiCalls'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Marvel
         </p>
         <a
           className="App-link"
