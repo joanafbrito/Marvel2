3325ac143681bc1bf3685721b34fa099465727e4
Joana Brito <joanafiorentini@hotmail.com>
2021-09-26 00:57:38 -0700

    Made the api call to work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { getData } from '../../utils/apiCalls'
+// import { getData } from '../../utils/apiCalls'
+import { sampleData } from '../../utils/sampledata';
+import AllCharacter from '../AllCharacter/AllCharacter';
+import axios from 'axios';
 
 const App = () => {
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
 
-  useEffect(() => {
-    getCharactersData();
-  }, []);
+    useEffect(() => {
+      getData()
+    }, []);
 
-  const getCharactersData = async () => {
-    try {
-      const res = await getData()
-      const charactersData = await res.json()
-      setCharacters(charactersData.data.results)
-    } catch (err) {
-      console.log("Error: ", err)
-    }
-  };
-  console.log(characters)
+    const getData = async () => {
+        let pbk = '9b29a8325371bc7becbf4068fd0948eb';
+        let url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${pbk}`;
+
+        
+        return axios.get(url)
+                    .then(res => {
+                      setCharacters(res.data.data.results)})
+    }        
 
   return (
     <div className="App">
@@ -26,6 +28,7 @@ const App = () => {
         <p>
           Marvel 
         </p>
+        <AllCharacter characterData={ characters}/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import MD5 from 'md5';
+// import React from 'react';
+// import axios from "axios";
 
 
-export const getData = () => {
-    let pbK = '9b29a8325371bc7becbf4068fd0948eb';
-    let priK = '67a0383c546a155af724cb93270d79e5a8119c9e';
-    // let urlId = `https://gateway.marvel.com:443/v1/public/characters/1009664?apikey=${publicKey}`
-    let url = 'https://gateway.marvel.com:443/v1/public/characters';
-    let ts = Date.now();
-    let hash = MD5(ts + priK + pbK).toString();
-    let characterId = '1009664'; // Thor Id
-    
-    
-   
-    return fetch(url, {
-        ts: ts,
-        apiKey: pbK,
-        hash: hash,
-        characters: characterId
-    })
-           .then(res => res.ok ? res.json() : console.log(res))
-           .then(data => console.log(data))
-           console.log(hash)
-          
-}
+// export const getData = async () => {
+//     let pbk = '9b29a8325371bc7becbf4068fd0948eb';
+//     let url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${pbk}`;
+     
+     
+//     return axios.get(url)
+//                 .then(response => response.ok ? response.json() : console.log( '000000000000',response))
+//                 // .then(data => console.log("this is after the promise ",data))
+
+//                 // console.log(data)
+        
+//     // console.log("I'm in the api call %%%%%%%",response.data.data.results)                  
+// }
