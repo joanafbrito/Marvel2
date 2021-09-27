e5762eaff02699000c259d50a9e4aff7044ade0f
Joana Brito <joanafiorentini@hotmail.com>
2021-09-26 18:29:13 -0700

    Add Search component to app and send props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 // import { getData } from '../../utils/apiCalls'
-import { sampleData } from '../../utils/sampledata';
-import Header from '../Header/Header';
-import AllCharacter from '../AllCharacter/AllCharacter';
-import axios from 'axios';
+import { sampleData } from "../../utils/sampledata";
+import Header from "../Header/Header";
+import AllCharacter from "../AllCharacter/AllCharacter";
+import axios from "axios";
+import Search from "../Search/Search";
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState("");
+  const [searchedCharacter, setSearchedCharacter] = useState([]);
+  const [error, setError] = useState("");
 
-    useEffect(() => {
-      getData()
-    }, []);
+  useEffect(() => {
+    getData();
+  }, [query]);
 
-    const getData = async () => {
-        let pbk = '9b29a8325371bc7becbf4068fd0948eb';
-        let url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&series=354&apikey=${pbk}`;
-     
-        return axios.get(url)
-                    .then(res => {
-                      setCharacters(res.data.data.results)})
-    }        
+  const getData = async () => {
+    let pbk = "9b29a8325371bc7becbf4068fd0948eb";
+    let url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&series=354&apikey=${pbk}`;
+
+    if (query.length >= 3) {
+      url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${pbk}&nameStartsWith=${query}`;
+    }
+
+    return axios.get(url).then((res) => {
+      setCharacters(res.data.data.results);
+    });
+  };
+
+  const searchCharacter = (query) => {
+    const lowerCaseIput = query.toLowerCase();
+    if (lowerCaseIput.length >= 3) {
+      return setQuery(lowerCaseIput);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <AllCharacter characterData={ characters }/>
+        <Search searchCharacter={searchCharacter} />
+        <AllCharacter characterData={characters} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
-
