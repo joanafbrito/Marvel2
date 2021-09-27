22bbbf55d18dc4f5f02828d3c694af3d8c5196bf
Joana Brito <joanafiorentini@hotmail.com>
2021-09-26 20:37:16 -0700

    Add Style to Search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,9 +3,7 @@ import './Search.css';
 
 
 const Search = ({ searchCharacter }) => {
-    // const [query, setQuery] = useState('');
 
-    // searchCharacter(query);
     const handleChange = (e) => {
         e.preventDefault()
         searchCharacter(e.target.value)
@@ -18,14 +16,16 @@ const Search = ({ searchCharacter }) => {
                     <img className='home-page-bg' src="https://cdn.guidingtech.com/imager/assets/191091/Best-HD-Wallpapers-From-the-Marvel-Universe-5_935adec67b324b146ff212ec4c69054f.webp" alt='Marvel background'/>
                 </section>
             </div>
-            <input
-              className='search-bar'
-              type='search'
-              placeholder='SEARCH'
-            //   onChange={(e)=> searchCharacter(e.target.value)}
-              onChange={(e)=> handleChange(e)}
-
-            />
+            <div className='search-bar-container'>
+                <img className='icon-search' src='/search_icon.svg' alt='Search icon'/>
+                <input
+                className='search-bar'
+                type='search'
+                placeholder='SEARCH'
+                onChange={(e)=> handleChange(e)}
+                />
+            </div>
+            <div className='gray-line'></div>
         </form>
     )
 }
