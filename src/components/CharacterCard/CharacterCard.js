70bc27e55c0ff29d207ad3199f709de26e6eaf4c
Joana Brito <joanafiorentini@hotmail.com>
2021-09-26 22:30:24 -0700

    Add Link to character card

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './CharacterCard.css';
-
+import React from "react";
+import "./CharacterCard.css";
+import { Link } from "react-router-dom";
 
 const CharacterCard = (props) => {
-    return (
-        <section className='character-card'>
-            <div className='card-container'>
-                <img className='card-img' src={ props.image }  alt={props.name}/>
-                <div className='line'></div>
-                <h3>{ props.name }</h3>
-            </div>
-        </section>
-    )
-}
+  return (
+    <Link to={`/character/${props.id}`}>
+      <section className="character-card">
+        <div className="card-container">
+          <img className="card-img" src={props.image} alt={props.name} />
+          <div className="line"></div>
+          <h3>{props.name}</h3>
+        </div>
+      </section>
+    </Link>
+  );
+};
 
 export default CharacterCard;
